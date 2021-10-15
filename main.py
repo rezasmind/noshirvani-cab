82fec3d0fbeac7d3b93ce960b0a162d4273d7ac7
Reza Aghajani <rezaaghajani@protonmail.com>
2021-10-16 01:49:31 +0330

    Login to the VIANA

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+
 
 
 driver = webdriver.Chrome(executable_path='/home/daddy/chromedriver')
@@ -13,6 +15,11 @@ driver.maximize_window()
 sleep(2)
 driver.get('http://elearning1.nit.ac.ir/') 
 print(driver.title)
-sleep(5)
+sleep(12)
+username = driver.find_element(By.ID,"username")
+password = driver.find_element(By.ID,"password")
 
-driver.quit()
+username.send_keys("username")
+password.send_keys("password")
+password.send_keys(Keys.RETURN)
+
